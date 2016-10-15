485e3e8e540f14ae30f464bf40ecfc52d3b7d939
Yura Trambitskiy <tr.yura@yahoo.com>
2016-10-15 14:02:25 +0300

    Connect scss, change css architecture to pure css

--- Main.elm
+++ Main.elm
@@ -96,7 +96,7 @@ view model =
             div [class "container app-container text-center"] [
                 div [class "row"] [
                     div [class "col-sm-12 col-xs-12"] [
-                        div [class "word"] [
+                        div [class "word-"] [
                             div [class "word-body"] [
                                 text givenWord
                             ],
